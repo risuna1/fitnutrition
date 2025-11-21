dc84b1a6b4cbe22817eee5faa1b5d7a85fc9eff2
Risuna <51692915+risuna1@users.noreply.github.com>
2025-11-21 09:42:06 +0900

    v2.2

--- app/backend/apps/analytics/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-# Analytics app doesn't need models as it will use data from other apps
-# All calculations will be done in views/services
--- app/backend/apps/measurements/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Measurements App - Handles body measurements tracking
-"""
-default_app_config = 'apps.measurements.apps.MeasurementsConfig'
